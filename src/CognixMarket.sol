0a876cea7abf4786d17bbbcae325f1d79485459d
dimka90 <dimkayilrit@gmail.com>
2026-01-12 23:43:52 +0100

    feat: add arbitrator state and constructor

--- src/CognixMarket.sol
+++ src/CognixMarket.sol
@@ -5,5 +5,10 @@ import {ICognixMarket} from "./interfaces/ICognixMarket.sol";
 
 contract CognixMarket is ICognixMarket {
     uint256 public taskCount;
+    address public arbitrator;
     mapping(uint256 => Task) public tasks;
+
+    constructor() {
+        arbitrator = msg.sender;
+    }
 }
