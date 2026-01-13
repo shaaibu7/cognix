97871788d40496b585c4ad32f4737e551756cdc9
dimka90 <dimkayilrit@gmail.com>
2026-01-13 14:58:51 +0100

    chore: integrate OpenZeppelin SafeERC20 for secure token handling

--- src/CognixMarket.sol
+++ src/CognixMarket.sol
@@ -3,10 +3,12 @@ pragma solidity ^0.8.19;
 
 import {ICognixMarket} from "./interfaces/ICognixMarket.sol";
 import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract CognixMarket is ICognixMarket, ReentrancyGuard, Ownable {
+    using SafeERC20 for IERC20;
     using SafeERC20 for IERC20;
 
     uint256 public taskCount;
