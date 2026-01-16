60ea6e2adc499a9230205fa7c43a686ba53ba303
shaaibu7 <shaaibusuleiman9@gmail.com>
2026-01-16 13:06:53 +0100

    docs: add contract title documentation

--- src/CognixToken.sol
+++ src/CognixToken.sol
@@ -1,12 +1,56 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
+
+/// @title Cognix Token - ERC20 Implementation
 contract CognixToken {
     string public name = "Cognix Token";
     string public symbol = "CGX";
     uint8 public decimals = 18;
     uint256 public totalSupply;
+    
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+    
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    
     constructor(string memory _name, string memory _symbol, uint256 _supply, address _owner) {
-        name = _name; symbol = _symbol; totalSupply = _supply; balanceOf[_owner] = _supply;
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _supply;
+        balanceOf[_owner] = _supply;
+        emit Transfer(address(0), _owner, _supply);
+    }
+    
+    function transfer(address to, uint256 amount) external returns (bool) {
+        require(to != address(0), "Invalid recipient");
+        require(balanceOf[msg.sender] >= amount, "Insufficient balance");
+        
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+        
+        emit Transfer(msg.sender, to, amount);
+        return true;
+    }
+    
+    function approve(address spender, uint256 amount) external returns (bool) {
+        require(spender != address(0), "Invalid spender");
+        
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+    
+    function transferFrom(address from, address to, uint256 amount) external returns (bool) {
+        require(to != address(0), "Invalid recipient");
+        require(balanceOf[from] >= amount, "Insufficient balance");
+        require(allowance[from][msg.sender] >= amount, "Insufficient allowance");
+        
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        allowance[from][msg.sender] -= amount;
+        
+        emit Transfer(from, to, amount);
+        return true;
     }
 }
