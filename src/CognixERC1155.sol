0302a8eed502c108653811143aa7d6914dac2bc4
shaaibu7 <shaaibusuleiman9@gmail.com>
2026-01-23 05:44:58 +0100

    feat: implement _doSafeBatchTransferAcceptanceCheck function

--- src/CognixERC1155.sol
+++ src/CognixERC1155.sol
@@ -268,4 +268,30 @@ contract CognixERC1155 is ERC165, IERC1155 {
             }
         }
     }
+    
+    /**
+     * @dev Internal function to check transfer acceptance for batch transfers
+     */
+    function _doSafeBatchTransferAcceptanceCheck(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) private {
+        if (to.code.length > 0) {
+            try IERC1155Receiver(to).onERC1155BatchReceived(operator, from, ids, amounts, data) returns (
+                bytes4 response
+            ) {
+                if (response != IERC1155Receiver.onERC1155BatchReceived.selector) {
+                    revert("ERC1155: ERC1155Receiver rejected tokens");
+                }
+            } catch Error(string memory reason) {
+                revert(reason);
+            } catch {
+                revert("ERC1155: transfer to non ERC1155Receiver implementer");
+            }
+        }
+    }
 }
