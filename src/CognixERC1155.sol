f8d3258f4e4e63706b17b8fcf5ef7d87cda671c8
shaaibu7 <shaaibusuleiman9@gmail.com>
2026-01-23 05:40:29 +0100

    feat: create initial ERC1155 contract file

---
+++ src/CognixERC1155.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/**
+ * @title CognixERC1155
+ * @dev Basic ERC1155 implementation
+ */
+contract CognixERC1155 {
+    // Initial contract structure
+}
