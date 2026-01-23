957158cba28e14691cc4cae21db7a40b6f55b5a0
shaaibu7 <shaaibusuleiman9@gmail.com>
2026-01-23 05:40:43 +0100

    feat: add ERC1155 interface imports and inheritance

--- src/CognixERC1155.sol
+++ src/CognixERC1155.sol
@@ -1,10 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+
 /**
  * @title CognixERC1155
  * @dev Basic ERC1155 implementation
  */
-contract CognixERC1155 {
+contract CognixERC1155 is ERC165, IERC1155 {
     // Initial contract structure
 }
